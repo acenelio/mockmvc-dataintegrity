d51a33291aa3e05128450a5589b6219d4123e947
Nelio Alves <acenelio@gmail.com>
2021-05-16 19:02:20 -0300

    Tests

---
+++ src/test/java/com/devsuperior/bds02/Bds02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bds02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bds02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
